80a697f9659d0b6925e6824db59ff0ea762c2d4a
Sam Kester <samkester@gmail.com>
2020-09-29 13:48:52 -0600

    Animal abstract class

---
+++ src/animalKingdom/Animal.java
@@ -0,0 +1,28 @@
+package animalKingdom;
+
+public abstract class Animal
+{
+    public static int nextID = 0;
+
+    private int id;
+    private String name;
+    private int year;
+
+    public Animal(String name, int year)
+    {
+        this.name = name;
+        this.year = year;
+
+        id = nextID;
+        nextID++;
+    }
+
+    public String eats()
+    {
+        return "Plants";
+    }
+
+    abstract public String moves();
+    abstract public String breathes();
+    abstract public String reproduces();
+}
